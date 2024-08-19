d56774dbb9815766e7a1c260ed9fe85530f643d8
Nikolai Aksenov <inikolay.axenov@gmail.com>
2024-08-19 16:21:39 +0300

    fix

--- app/ui/Root/Root.tsx
+++ app/ui/Root/Root.tsx
@@ -14,10 +14,10 @@ import {
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 
-import { ErrorBoundary } from '@/components/ErrorBoundary';
-import { ErrorPage } from '@/components/ErrorPage';
-import { useTelegramMock } from '@/hooks/useTelegramMock';
-import { useDidMount } from '@/hooks/useDidMount';
+import { ErrorBoundary } from '../ErrorBoundary'; 
+import { ErrorPage } from '../ErrorPage';
+import { useTelegramMock } from '@/app/libs/hooks/useTelegramMock';
+import { useDidMount } from '@/app/libs/hooks/useDidMount';
 
 import './styles.css';
 
